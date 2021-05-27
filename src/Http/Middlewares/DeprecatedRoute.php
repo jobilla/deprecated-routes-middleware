984d7f80444ce00e84f649a919ca4dd8b8d86454
Hakan Aktas <iletisim@hakanaktas.com.tr>
2021-05-27 10:12:58 +0300

    truthy check for event config

--- src/Http/Middlewares/DeprecatedRoute.php
+++ src/Http/Middlewares/DeprecatedRoute.php
@@ -53,7 +53,7 @@ class DeprecatedRoute
     {
         $enabled = Config::get('deprecated_routes.fire_event');
 
-        if ($enabled !== true) {
+        if ($enabled) {
             return;
         }
 
